376cb68db72c19443cd18de2de401d22e5d23f64
Sean Welch <seanwelch51199@icloud.com>
2023-11-27 19:41:38 +0000

    rendered country data on front end

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,23 @@
+import useFetchCountries from '../hooks/fetchCountries';
+import { Country } from '../types/country';
+
 const Home = () => {
-  return <h1>Hello</h1>;
+  const { countries, isLoading, error } = useFetchCountries();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <div>
+      <h1>Hello</h1>
+
+      <ul>
+        {countries.map((country: Country) => (
+          <li key={country?.cca2}>{country.name.common}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
