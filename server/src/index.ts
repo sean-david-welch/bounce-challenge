4e28768fd71867ac5740c1de686dc345b7e2533f
Sean Welch <seanwelch51199@icloud.com>
2023-11-27 12:47:38 +0000

    installed express, cors etc && types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello typesript');
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
